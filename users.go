40343650ebd7a5198f5afcfbbcfae50eb34e2fc6
Abdul Halim <abdul.halim@bs-23.net>
2022-03-02 22:30:19 +0600

    connection file added

---
+++ users.go
@@ -0,0 +1,14 @@
+package users
+
+type User struct {
+	ID       int
+	Username string
+}
+
+type Userdata struct {
+	ID          int
+	Username    string
+	Name        string
+	Surname     string
+	Description string
+}
